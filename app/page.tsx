a185259e06ff95df556ffca8dc5f982ad5c0053e
Mark Outcalt <mtoutcalt@gmail.com>
2024-11-12 21:34:51 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,6 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>WORK IN PROGRESS</h1>
-            <Image
-              src="/liampirate.jpg"
-              alt="liam pirate"
-              priority
-            />
-            <Image
-              src="/younglove.jpg"
-              alt="young love"
-              priority
-            />
     </main>
   );
 }

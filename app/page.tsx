0a1d74ec2fa6cd07dbbf3af6f08ddac9e1cc0bdc
Mark Outcalt <mtoutcalt@gmail.com>
2024-11-12 21:33:02 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>HI ROBYN and LIAM!</h1>
+      <h1>WORK IN PROGRESS</h1>
             <Image
               src="/liampirate.jpg"
               alt="liam pirate"

0d9fd794ffd061206c163daf323bda0de5196ca1
Mark Outcalt <mtoutcalt@gmail.com>
2025-05-17 09:17:03 -0400

    Update rss.xml.js
    remove duplicate...dumb

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,19 +3,6 @@ import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.id}/`,
-		})),
-	});
-}
-
-export async function GET(context) {
   try {
     const posts = await getCollection('blog');
     return rss({

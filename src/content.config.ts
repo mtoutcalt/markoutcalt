0fa589f41b74fcd15365f2f1b9f768dbc171afdb
Mark Outcalt <mtoutcalt@gmail.com>
2025-05-29 18:01:42 -0400

    get rid of notes

--- src/content.config.ts
+++ src/content.config.ts
@@ -15,17 +15,17 @@ const blog = defineCollection({
 	}),
 });
 
-const notes = defineCollection({
-	loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/content/notes" }),
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-		tags: z.array(z.string())
-	}),
-});
+// const notes = defineCollection({
+// 	loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/content/notes" }),
+// 	schema: z.object({
+// 		title: z.string(),
+// 		description: z.string(),
+// 		// Transform string to Date object
+// 		pubDate: z.coerce.date(),
+// 		updatedDate: z.coerce.date().optional(),
+// 		heroImage: z.string().optional(),
+// 		tags: z.array(z.string())
+// 	}),
+// });
 
-export const collections = { blog, notes };
+export const collections = { blog };

dbb25c8a6e333849a26f8cab30fc86beb176ce27
Mark Outcalt <mtoutcalt@gmail.com>
2025-06-22 17:18:14 -0400

    quote files for programming

---
+++ src/data/computer-quotes.js
@@ -0,0 +1,22 @@
+export const programmingQuotes = [
+    {
+      "content": "It is not usually until you’ve built and used a version of the program that you understand the issues well enough to get the design right.",
+      "source": "Kernighan and Pike"
+    },
+    {
+      "content": "The best performance improvement is the transition from the nonworking state to the working state.",
+      "source": "John Ousterhout"
+    },
+    {
+      "content": "You don’t pay engineers to write code, you pay them to understand subtleties and edges of the problem. The code is incidental.",
+      "source": "@dozba"
+    },
+    {
+      "content": "The art of debugging is figuring out what you really told your program to do rather than what you thought you told it to do.",
+      "source": "Andrew Singer"
+    },
+    {
+      "content": "The fascination of fashioning complex puzzle-like objects of interlocking moving parts, and watching them work in subtle cycles, playing out the consequences of principles built in from the beginning. The delight of working in such a tractable medium. The programmer, like the poet, works only slightly removed from pure thought-stuff. He builds his castles in the air, from air, creating by exertion of imagination",
+      "source": "Fred Brooks"
+    },
+]

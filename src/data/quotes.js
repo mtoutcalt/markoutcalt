909a5c6ed2bbd9a90471db6ac6e5a0feb371ec8b
Mark Outcalt <mtoutcalt@gmail.com>
2025-10-17 17:27:38 -0400

    Update quotes.js

--- src/data/quotes.js
+++ src/data/quotes.js
@@ -619,6 +619,16 @@ export const quotes = [
     id: 119,
     text: "Whoever has the most fun, wins",
     author: "Anonymous"
+  },
+  {
+    id: 120,
+    text: "If you find yourself struggling to build a good habit or break a bad one, it is not because you have lost your ability to improve. It is often because you have not yet crossed the Plateau of Latent Potential. Complaining about not achieving success despite working hard is like complaining about an ice cube not melting when you heated it from twenty-five to thirty-one degrees. Your work was not wasted; it is just being stored. All the action happens at thirty-two degrees.",
+    author: "James Clear"
+  },
+  {
+    id: 121,
+    text: "The ceramics teacher announced on opening day that he was dividing the class into two groups. All those on the left side of the studio, he said, would be graded solely on the quantity of work they produced, all those on the right solely on its quality. His procedure was simple: on the final day of class he would bring in his bathroom scales and weigh the work of the 'quantity' group: fifty pound of pots rated an 'A', forty pounds a 'B', and so on. Those being graded on 'quality', however, needed to produce only one pot – albeit a perfect one – to get an 'A'. Well, came grading time and a curious fact emerged: the works of highest quality were all produced by the group being graded for quantity. It seems that while the 'quantity' group was busily churning out piles of work – and learning from their mistakes – the 'quality' group had sat theorizing about perfection, and in the end had little more to show for their efforts than grandiose theories and a pile of dead clay.",
+    author: "Art & Fear by David Bayles and Ted Orland"
   }
 ];
 

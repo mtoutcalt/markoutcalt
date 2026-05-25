925ff4daca2e7b20ab04abd8f3f5d0536b28a4a8
Mark Outcalt <mtoutcalt@gmail.com>
2026-05-25 19:45:10 -0400

    quote

--- src/data/quotes.js
+++ src/data/quotes.js
@@ -724,6 +724,11 @@ export const quotes = [
     id: 139,
     text: `Hope is definitely not the same thing as optimism. It is not the conviction that something will turn out well, but the certainty that something makes sense, regardless of how it turns out.`,
     author: 'Václav Havel'
+  },
+  {
+    id: 140,
+    text: `"Oh, she says, well, you're not a poor man. You know, why don't you go online and buy a hundred envelopes and put them in the closet? And so I pretend not to hear her. And go out to get an envelope because I'm going to have a hell of a good time in the process of buying one envelope. I meet a lot of people. And see some great looking babies. And a fire engine goes by. And I give them the thumbs up. And I'll ask a woman what kind of dog that is. And, and I don't know. The moral of the story is we're here on Earth to fart around. And, of course, the computers will do us out of that. And what the computer people don't realize, or they don't care, is we're dancing animals. You know, we love to move around. And it's like we're not supposed to dance anymore."`,
+    author: 'Kurt Vonnegut'
   }
 ];
 
